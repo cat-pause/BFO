401f1919b12beb57aa0a81ebdf8b1f95a9fcc2cc
wonhee <asdkcone13@gmail.com>
2022-10-06 12:32:10 +0900

    add login/join.html

---
+++ script/join.js
@@ -0,0 +1,16 @@
+$(function(){
+
+    const email = document.querySelector("#email");
+    const eBtn = document.querySelector("#email_check");
+
+    email.addEventListener('keyup',e=>{
+        if(!!e.target.value){
+            console.log("입력");
+            eBtn.style.cssText="background-color:#35C5F0;color:#fff;border:none";
+        }else{
+            console.log("else");
+            eBtn.style.cssText="background-color:none";
+        }
+    })
+
+})
