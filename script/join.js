9d2a0dc410fca182c6240c5ce09608537594d083
wonhee <asdkcone13@gmail.com>
2022-10-24 14:03:55 +0900

    join.js ! 수정

--- script/join.js
+++ script/join.js
@@ -4,7 +4,7 @@ $(function(){
     const eBtn = document.querySelector("#email_check");
 
     email.addEventListener('keyup',e=>{
-        if(!!e.target.value){
+        if(e.target.value){
             console.log("입력");
             eBtn.style.cssText="background-color:#35C5F0;color:#fff;border:none";
         }else{
